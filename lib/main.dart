a6fcd996522c9e39db00ee8ec3c9bd8728c4a388
marek-jakub <mj3935538@gmail.com>
2023-05-22 16:31:42 +0200

    Changing app name to MonitoRing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MonitoRing());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MonitoRing extends StatelessWidget {
+  const MonitoRing({super.key});
 
   @override
   Widget build(BuildContext context) {
