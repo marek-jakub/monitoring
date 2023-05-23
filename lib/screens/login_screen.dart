949c59551671b675231ae45b5d923f538a6135aa
marek-jakub <mj3935538@gmail.com>
2023-05-23 15:57:06 +0200

    Adding login screen.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/models.dart';
+
+class LoginScreen extends StatelessWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.loginPath,
+      key: ValueKey(MonitoRingPages.loginPath),
+      child: const LoginScreen(),
+    );
+  }
+
+  final String? username;
+
+  const LoginScreen({
+    Key? key,
+    this.username,
+  }) : super(key: key);
+
+  final Color ringsColor = const Color.fromRGBO(46, 134, 77, 1);
+  final TextStyle onFocusStyle = const TextStyle(color: Colors.green);
+  final TextStyle nonFocusStyle = const TextStyle(color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // TODO: create sign in GUI
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
