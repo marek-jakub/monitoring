aa85c0f8c06e0923b1675abd85e90b69a593f387
marek-jakub <mj3935538@gmail.com>
2023-05-23 14:30:12 +0200

    Adding manager.

--- lib/navigation/monitoring_router.dart
+++ lib/navigation/monitoring_router.dart
@@ -10,4 +10,48 @@ class MonitoRingRouter extends RouterDelegate<MonitoRingLink>
   final GlobalKey<NavigatorState> navigatorKey;
 
   final MonitoRingStateManager monStateManager;
+  final ProfileManager profileManager;
+  final RingDataManager ringDataManager;
+
+  MonitoRingRouter({
+    required this.monStateManager,
+    required this.profileManager,
+    required this.ringDataManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    monStateManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+    ringDataManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    monStateManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    ringDataManager.removeListener(notifyListeners);
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+        // TODO: add path to screens
+        );
+  }
+
+  @override
+  Future<void> setNewRoutePath(MonitoRingLink configuration) async {
+    switch (configuration.location) {
+      case MonitoRingLink.keyProfilePath:
+        profileManager.tapOnProfile(true);
+        break;
+      case MonitoRingLink.keyHomePath:
+        monStateManager.goToTab(configuration.currentTab ?? 0);
+        profileManager.tapOnProfile(false);
+        break;
+      // TODO: Add paths to session, ring
+      default:
+        break;
+    }
+  }
 }
