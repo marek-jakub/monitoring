bd44155660e1a14a8aa9476858b51383663ad617
marek-jakub <mj3935538@gmail.com>
2023-05-23 15:38:05 +0200

    Adding file holding app page paths.

---
+++ lib/models/monitoring_pages.dart
@@ -0,0 +1,6 @@
+class MonitoRingPages {
+  static String splashPath = '/splash';
+  static String loginPath = '/login';
+  static String onboardingPath = '/onboarding';
+  static String home = '/';
+}
