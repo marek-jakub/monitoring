1387fbc74f63551e5364cf08b9c31c0a5b378d99
marek-jakub <mj3935538@gmail.com>
2023-05-24 17:37:05 +0200

    Add database.

--- lib/models/ring_data_manager.dart
+++ lib/models/ring_data_manager.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:monitoring/data/monitoring_db.dart';
 
 /// Notifier for ringing data and database access.
 ///
 /// Allows main management of ringing data - sessions, rings and retraps.
 class RingDataManager extends ChangeNotifier {
-  // TODO: create database access logic
+  /// App database
+  MonRingDb? _monRingDb;
+
+  void initDb(MonRingDb db) {
+    _monRingDb = db;
+  }
 }
