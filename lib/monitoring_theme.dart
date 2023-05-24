465b5345e6c3bc87e0d53aeeb7cf6003640d5d10
marek-jakub <mj3935538@gmail.com>
2023-05-24 17:37:32 +0200

    Add class definition.

--- lib/monitoring_theme.dart
+++ lib/monitoring_theme.dart
@@ -1,1 +1,19 @@
-// TODO: create light and dark theme for the app
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// Holds application theme definition.
+///
+/// Two definitions, light and dark mode.
+class MonitoRingTheme {
+  static TextTheme lightTextTheme = TextTheme();
+  static TextTheme darkTextTheme = TextTheme();
+
+  // TODO: define light and dark mode settings
+  static ThemeData light() {
+    return ThemeData();
+  }
+
+  static ThemeData dark() {
+    return ThemeData();
+  }
+}
