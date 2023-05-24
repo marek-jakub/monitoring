eb9927db6d77d33b6b4e72212515d65bfb27db24
marek-jakub <mj3935538@gmail.com>
2023-05-24 16:35:28 +0200

    Add doc comments.

--- lib/models/ringer.dart
+++ lib/models/ringer.dart
@@ -1,5 +1,9 @@
+/// Ringer information and profile settins.
 class Ringer {
+  /// The user ID.
   final String ringerId;
+
+  /// The value of dark mode selection.
   final bool darkMode;
 
   Ringer({
