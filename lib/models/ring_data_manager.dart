f3a023217a354689d32a4e3d0cb464048a279832
marek-jakub <mj3935538@gmail.com>
2023-05-24 16:21:54 +0200

    Add doc comments.

--- lib/models/ring_data_manager.dart
+++ lib/models/ring_data_manager.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
+/// Notifier for ringing data and database access.
+///
+/// Allows main management of ringing data - sessions, rings and retraps.
 class RingDataManager extends ChangeNotifier {
   // TODO: create database access logic
 }
