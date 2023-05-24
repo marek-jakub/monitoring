55f3093f31330ea9239618f0e8e3bea409439865
marek-jakub <mj3935538@gmail.com>
2023-05-24 16:33:44 +0200

    Add doc comments.

--- lib/models/profile_manager.dart
+++ lib/models/profile_manager.dart
@@ -2,32 +2,43 @@ import 'package:flutter/material.dart';
 
 import 'models.dart';
 
+/// Notifier and manager of ringer's profile.
+///
+/// Currently holds mock data.
 class ProfileManager extends ChangeNotifier {
   Ringer get getRinger => Ringer(
         ringerId: '6000',
         darkMode: _darkMode,
       );
 
+  /// User selected value on tapping the profile.
+  ///
+  /// Returns true of user selects profile settings.
   bool get didSelectRinger => _didSelectRinger;
+  void tapOnProfile(bool selected) {
+    _didSelectRinger = selected;
+    notifyListeners();
+  }
+
+  /// User selected value of app mode.
+  ///
+  /// Returns true if user selects dark mode.
   bool get darkMode => _darkMode;
-  bool get didTapOnEuringCode => _tapOnEuringCode;
-
-  var _didSelectRinger = false;
-  var _darkMode = false;
-  var _tapOnEuringCode = false;
-
   set darkMode(bool darkMode) {
     _darkMode = darkMode;
     notifyListeners();
   }
 
+  /// User selected value of Euring Code access.
+  ///
+  /// Returns true if user taps on Euring Code file.
+  bool get didTapOnEuringCode => _tapOnEuringCode;
   void tapOnEuringCode(bool selected) {
     _tapOnEuringCode = selected;
     notifyListeners();
   }
 
-  void tapOnProfile(bool selected) {
-    _didSelectRinger = selected;
-    notifyListeners();
-  }
+  var _didSelectRinger = false;
+  var _darkMode = false;
+  var _tapOnEuringCode = false;
 }
