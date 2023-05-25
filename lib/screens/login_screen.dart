66a2f0b781ab09bf5c40ef0633b577881508bf12
marek-jakub <mj3935538@gmail.com>
2023-05-25 15:36:11 +0200

    Adding username, password fields and login button.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,7 +15,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  /// Holds ringer ID.
+  /// Holds ringer username.
   final String? username;
 
   const LoginScreen({
@@ -37,10 +37,61 @@ class LoginScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // TODO: create sign in GUI
+              // TODO: add app image
+              const SizedBox(
+                height: 16.0,
+              ),
+              entryTextfield(username ?? 'username'),
+              const SizedBox(
+                height: 16.0,
+              ),
+              entryTextfield('password'),
+              const SizedBox(
+                height: 16.0,
+              ),
+              entryButton(context),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  /// A textfield for entering username.
+  Widget entryTextfield(String aHint) {
+    return TextField(
+      cursorColor: ringsColor,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.green,
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.green),
+        ),
+        hintText: aHint,
+        hintStyle: const TextStyle(height: 0.5),
+      ),
+    );
+  }
+
+  /// A button to log in the application.
+  Widget entryButton(BuildContext context) {
+    return SizedBox(
+      height: 45,
+      child: MaterialButton(
+        color: ringsColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
+        child: const Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () async {
+          Provider.of<MonitoRingStateManager>(context, listen: false)
+              .login('mockUsername', 'mockPassword');
+        },
       ),
     );
   }
