4a9740697b1d1cb0d3fe26c78f4125ff93a55be3
marek-jakub <mj3935538@gmail.com>
2023-05-25 16:11:23 +0200

    First commit.

--- lib/screens/retraps_screen.dart
+++ lib/screens/retraps_screen.dart
@@ -1,1 +1,29 @@
-// TODO: build retraps screen
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../data/monitoring_db.dart';
+import '../models/models.dart';
+
+class RetrapsScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.retrapsPath,
+      key: ValueKey(MonitoRingPages.retrapsPath),
+      child: const RetrapsScreen(),
+    );
+  }
+
+  const RetrapsScreen({super.key});
+
+  @override
+  State<RetrapsScreen> createState() => _RetrapsScreenState();
+}
+
+class _RetrapsScreenState extends State<RetrapsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // TODO: build retraps GUI
+        );
+  }
+}
