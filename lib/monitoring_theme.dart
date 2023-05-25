2568e99a35eeceb0318ad765c6395542e198acc2
marek-jakub <mj3935538@gmail.com>
2023-05-25 15:17:42 +0200

    Add light text theme.

--- lib/monitoring_theme.dart
+++ lib/monitoring_theme.dart
@@ -5,7 +5,34 @@ import 'package:google_fonts/google_fonts.dart';
 ///
 /// Two definitions, light and dark mode.
 class MonitoRingTheme {
-  static TextTheme lightTextTheme = TextTheme();
+  static TextTheme lightTextTheme = TextTheme(
+    bodyLarge: GoogleFonts.openSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headlineLarge: GoogleFonts.openSans(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headlineMedium: GoogleFonts.openSans(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headlineSmall: GoogleFonts.openSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    titleMedium: GoogleFonts.openSans(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  );
+
   static TextTheme darkTextTheme = TextTheme();
 
   // TODO: define light and dark mode settings
