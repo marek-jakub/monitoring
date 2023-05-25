9b5cc81425ddfd7d954526bd2099dc3f44eab02e
marek-jakub <mj3935538@gmail.com>
2023-05-25 16:05:10 +0200

    Adding pages list.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/models.dart';
 import '../screens/screens.dart';
 
-/// Home screen holding information about ringing sessions.
+/// Home screen serving as a container for screens.
 class Home extends StatefulWidget {
   static MaterialPage page(int currentTab) {
     return MaterialPage(
@@ -28,6 +28,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  /// Holds pages for the main screen movement.
+  static List<Widget> pages = <Widget>[
+    // const SessionsScreen(),
+    // const RetrapsScreen(),
+    // const SearchScreen(),
+    // const MapScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
