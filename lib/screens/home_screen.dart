bbaa402e27024a8790b122011b7c5062b4fec3fb
marek-jakub <mj3935538@gmail.com>
2023-05-25 15:46:05 +0200

    Adding Home class.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,37 @@
-// TODO: create landing screen for main app view
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+/// Home screen holding information about ringing sessions.
+class Home extends StatefulWidget {
+  static MaterialPage page(int currentTab) {
+    return MaterialPage(
+      name: MonitoRingPages.home,
+      key: ValueKey(MonitoRingPages.home),
+      child: Home(
+        currentTab: currentTab,
+      ),
+    );
+  }
+
+  const Home({
+    Key? key,
+    required this.currentTab,
+  }) : super(key: key);
+
+  final int currentTab;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // TODO: build home screen body
+        );
+  }
+}
