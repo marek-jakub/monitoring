2de14145a340948d3b963cc4f3886c15602712b5
marek-jakub <mj3935538@gmail.com>
2023-05-25 16:04:52 +0200

    Adding first commit.

--- lib/screens/sessions_screen.dart
+++ lib/screens/sessions_screen.dart
@@ -1,1 +1,41 @@
-// TODO: build sessions screen
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../data/monitoring_db.dart';
+import '../models/models.dart';
+
+/// Screen holding information about ringing sessions.
+class SessionsScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.sessionsPath,
+      key: ValueKey(MonitoRingPages.sessionsPath),
+      child: const SessionsScreen(),
+    );
+  }
+
+  const SessionsScreen({super.key});
+
+  @override
+  State<SessionsScreen> createState() => _SessionsScreenState();
+}
+
+class _SessionsScreenState extends State<SessionsScreen> {
+  /// Holds id of selected session.
+  int _selectedId = -1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
