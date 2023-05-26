d8fd4c0a4daf8cc762aafec4544bff4c31204a00
marek-jakub <mj3935538@gmail.com>
2023-05-26 09:34:06 +0200

    Adding GUI body.

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -2,4 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-import 'package:mrings/models/models.dart';
+import '../models/models.dart';
+
+/// Screen for onboarding messages after first log in.
+class OnboardingScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.onboardingPath,
+      key: ValueKey(MonitoRingPages.onboardingPath),
+      child: const OnboardingScreen(),
+    );
+  }
+
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final controller = PageController();
+  final Color ringsColor = const Color.fromRGBO(46, 134, 77, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: const Text('Beginning to use the app'),
+        leading: GestureDetector(
+          child: const Icon(
+            Icons.chevron_left,
+            size: 30,
+          ),
+          onTap: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
