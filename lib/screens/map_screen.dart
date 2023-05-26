37dd49c8111a213ba958cc5bfc721dad480dca49
marek-jakub <mj3935538@gmail.com>
2023-05-26 09:09:08 +0200

    First commit.

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,1 +1,40 @@
-// TODO: build map screen
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+/// Map screen holding graphical data pertaining to ring positions.
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  MapScreenState createState() => MapScreenState();
+}
+
+class MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  // Initial map position
+  final CameraPosition position = const CameraPosition(
+    target: LatLng(51.4189, 0.1933),
+    zoom: 12,
+  );
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          mapType: MapType.hybrid,
+          initialCameraPosition: position,
+          compassEnabled: true,
+        ),
+      ),
+    );
+  }
+}
