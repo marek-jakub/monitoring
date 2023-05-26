d78fddbd93f68fcbf7ec838257c8eadec0890be6
marek-jakub <mj3935538@gmail.com>
2023-05-26 09:29:02 +0200

    First commit.

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import 'package:mrings/models/models.dart';
