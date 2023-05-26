0b25b7e58e708220f59177717419c3c27950c35e
marek-jakub <mj3935538@gmail.com>
2023-05-26 09:00:19 +0200

    First commit.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,1 +1,24 @@
-// TODO: build search screen
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('search_screen'),
+        ],
+      ),
+    );
+  }
+}
