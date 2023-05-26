15bc17fc2070d4fa84ada926d67ceddc8078dcf8
marek-jakub <mj3935538@gmail.com>
2023-05-26 10:19:33 +0200

    Adding profile screen GUI.

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,1 +1,53 @@
-// TODO: create profile screen.
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/models.dart';
+
+/// A screen holding ringer profile information.
+///
+/// Offers access to add ring series, lost rings and make orders screens.
+class ProfileScreen extends StatefulWidget {
+  static MaterialPage page(Ringer ringer) {
+    return MaterialPage(
+      name: MonitoRingPages.profilePath,
+      key: ValueKey(MonitoRingPages.profilePath),
+      child: ProfileScreen(ringer: ringer),
+    );
+  }
+
+  /// Holds ringer information.
+  final Ringer ringer;
+
+  const ProfileScreen({
+    Key? key,
+    required this.ringer,
+  }) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MonitoRing - ringer profile'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            Provider.of<ProfileManager>(context, listen: false)
+                .tapOnProfile(false);
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
