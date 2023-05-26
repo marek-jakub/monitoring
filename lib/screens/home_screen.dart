4a99b88e6b9f7486d0b6331fc0213482bb20fec3
marek-jakub <mj3935538@gmail.com>
2023-05-26 09:20:43 +0200

    Created GUI body and added probile button.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,16 +30,90 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   /// Holds pages for the main screen movement.
   static List<Widget> pages = <Widget>[
-    // const SessionsScreen(),
-    // const RetrapsScreen(),
-    // const SearchScreen(),
-    // const MapScreen(),
+    const SessionsScreen(),
+    const RetrapsScreen(),
+    const SearchScreen(),
+    const MapScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // TODO: build home screen body
-        );
+      appBar: AppBar(
+        title: Text(
+          'MonitoRing',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        centerTitle: true,
+        actions: [
+          profileButton(),
+        ],
+      ),
+      body: IndexedStack(
+        index: widget.currentTab,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: widget.currentTab,
+        onTap: (value) {
+          Provider.of<MonitoRingStateManager>(context, listen: false)
+              .goToTab(value);
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.inventory,
+                color: Colors.brown,
+              ),
+              activeIcon: Icon(
+                Icons.inventory,
+                color: Colors.amber,
+              ),
+              label: 'Sessions'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.join_left_sharp,
+                color: Colors.brown,
+              ),
+              activeIcon: Icon(Icons.join_left_sharp, color: Colors.amber),
+              label: 'Retraps'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.brown,
+              ),
+              activeIcon: Icon(
+                Icons.search,
+                color: Colors.amber,
+              ),
+              label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.map,
+                color: Colors.brown,
+              ),
+              activeIcon: Icon(
+                Icons.map,
+                color: Colors.amber,
+              ),
+              label: 'Maps'),
+        ],
+      ),
+    );
+  }
+
+  /// A button for ringer profile access.
+  Widget profileButton() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: IconButton(
+        icon: const Icon(Icons.settings),
+        onPressed: () {
+          Provider.of<ProfileManager>(context, listen: false)
+              .tapOnProfile(true);
+        },
+      ),
+    );
   }
 }
