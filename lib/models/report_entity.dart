0d35827be2a02af2b72fe673c2eeb67ded4cfbd8
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:17:57 +0200

    Adding ringing report model.

--- lib/models/report_entity.dart
+++ lib/models/report_entity.dart
@@ -1,1 +1,8 @@
-// TODO: create report entity.
+import 'package:drift/drift.dart';
+
+/// A model of bird ringing report.
+class ReportEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get date => text()();
+}
