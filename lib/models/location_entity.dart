998a1221d7f4b0529e87a963e9623b3eee6f9412
marek-jakub <mj3935538@gmail.com>
2023-05-28 15:36:09 +0200

    Defining location entity table.

--- lib/models/location_entity.dart
+++ lib/models/location_entity.dart
@@ -1,1 +1,14 @@
-// TODO: create location entity.
+import 'package:drift/drift.dart';
+
+class LocationEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get locality => text()();
+  TextColumn get placeCode => text()();
+  TextColumn get latitude => text()();
+  TextColumn get longitude => text()();
+  TextColumn get coordinatesAccuracy => text()();
+
+  // can be empty strings
+  TextColumn get localeInfo => text()();
+}
