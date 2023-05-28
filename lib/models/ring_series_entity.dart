41774f8c77563dce8812eb19976e566013efb9d3
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:32:01 +0200

    Adding ring series model.

--- lib/models/ring_series_entity.dart
+++ lib/models/ring_series_entity.dart
@@ -1,1 +1,11 @@
-// TODO: crate ring series entity.
+import 'package:drift/drift.dart';
+
+/// A model of ring series.
+class RingseriesEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get code => text()();
+  TextColumn get schemeCode => text()();
+  IntColumn get ringfrom => integer()();
+  IntColumn get ringto => integer()();
+}
