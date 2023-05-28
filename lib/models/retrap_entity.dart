de2559104931a6c8aa4592c4cf3b466b1a035a13
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:29:13 +0200

    Adding retrap ring model.

--- lib/models/retrap_entity.dart
+++ lib/models/retrap_entity.dart
@@ -1,1 +1,52 @@
-// TODO: create retrap entity.
+import 'package:drift/drift.dart';
+
+import '../models/models.dart';
+
+/// A model of retrap ring.
+class RetrapEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get sessionId => integer().references(SessionEntity, #id)();
+  TextColumn get ringSeriesCode => text()();
+  TextColumn get ringIdNumber => text()();
+  TextColumn get ringScheme => text()();
+  TextColumn get condition => text()();
+
+  // can be empty
+  TextColumn get primaryIdMethod => text()();
+  TextColumn get metalRingInfo => text()();
+  TextColumn get species => text()();
+  TextColumn get age => text()();
+  TextColumn get sex => text()();
+  TextColumn get sexingMethod => text()();
+  TextColumn get otherMarks => text()();
+  TextColumn get catchingMethod => text()();
+  TextColumn get catchingLures => text()();
+  TextColumn get circumstances => text()();
+  TextColumn get circumstancesPresumed => text()();
+  TextColumn get status => text()();
+  TextColumn get time => text()();
+  TextColumn get wingLength => text()();
+  TextColumn get thirdPrimary => text()();
+  TextColumn get wingPointState => text()();
+  TextColumn get mass => text()();
+  TextColumn get moult => text()();
+  TextColumn get plumageCode => text()();
+  TextColumn get hindClaw => text()();
+  TextColumn get billLength => text()();
+  TextColumn get billMethod => text()();
+  TextColumn get headLengthTotal => text()();
+  TextColumn get tarsus => text()();
+  TextColumn get tarsusMethod => text()();
+  TextColumn get tailLength => text()();
+  TextColumn get tailDifference => text()();
+  TextColumn get fatScore => text()();
+  TextColumn get fatScoreMethod => text()();
+  TextColumn get pectoralMuscle => text()();
+  TextColumn get broodPatch => text()();
+  TextColumn get primaryScore => text()();
+  TextColumn get primaryMoult => text()();
+  TextColumn get oldGreaterCoverts => text()();
+  TextColumn get alula => text()();
+  TextColumn get carpalCovert => text()();
+}
