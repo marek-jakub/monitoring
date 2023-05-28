e411a9d4eb2c219b0c3ebb695bdd4c12dd943ce2
marek-jakub <mj3935538@gmail.com>
2023-05-28 15:57:56 +0200

    Adding lost ring model.

--- lib/models/lost_ring_entity.dart
+++ lib/models/lost_ring_entity.dart
@@ -1,1 +1,9 @@
-// TODO: create lost ring entity.
+import 'package:drift/drift.dart';
+
+/// A model of lost ring.
+class LostRingEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get ringSeriesCode => text()();
+  TextColumn get idNumber => text()();
+}
