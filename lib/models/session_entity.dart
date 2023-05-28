d6771b6147ce9e94307462cd0c63ff095fbb0d12
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:20:33 +0200

    Adding session model.

--- lib/models/session_entity.dart
+++ lib/models/session_entity.dart
@@ -1,1 +1,17 @@
-// TODO: create session entity.
+import 'package:drift/drift.dart';
+
+import '../models/models.dart';
+
+/// A model of ringing session.
+class SessionEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get date => text()();
+  TextColumn get dateAccuracy => text()();
+  IntColumn get location => integer().references(LocationEntity, #id)();
+  TextColumn get ringerId => text()();
+
+  // can be empty
+  TextColumn get startTime => text()();
+  TextColumn get endTime => text()();
+}
