f11d50a78b3462951416b5b89ceb42f0f974b6fa
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:15:29 +0200

    Adding ring order model.

--- lib/models/order_entity.dart
+++ lib/models/order_entity.dart
@@ -1,1 +1,9 @@
-// TODO: create order entity.
+import 'package:drift/drift.dart';
+
+/// A model of ring order.
+class OrderEntity extends Table {
+  // non-nullable
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get ringSeriesCode => text()();
+  IntColumn get amount => integer()();
+}
