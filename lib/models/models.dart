b998a048fed23d6faaf2abd20833c6b4ca2866f2
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:26:33 +0200

    Adding report entity export.

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -5,6 +5,7 @@ export 'monitoring_pages.dart';
 export 'monitoring_state_manager.dart';
 export 'order_entity.dart';
 export 'profile_manager.dart';
+export 'report_entity.dart';
 export 'ring_data_manager.dart';
 export 'ringer.dart';
 export 'session_entity.dart';
