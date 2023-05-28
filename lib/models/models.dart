3640a9a36d83c00d398adebf3956bdc7bd634014
marek-jakub <mj3935538@gmail.com>
2023-05-28 16:25:54 +0200

    Adding order entity export.

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -3,6 +3,7 @@ export 'lost_ring_entity.dart';
 export 'monitoring_cache.dart';
 export 'monitoring_pages.dart';
 export 'monitoring_state_manager.dart';
+export 'order_entity.dart';
 export 'profile_manager.dart';
 export 'ring_data_manager.dart';
 export 'ringer.dart';
