4e2015458bb867d94fff4a04be6030c5cf28fa56
marek-jakub <mj3935538@gmail.com>
2023-05-28 17:14:22 +0200

    Rearranging exports in alphabetical order.

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,10 +1,10 @@
 export 'euring_code_screen.dart';
-export 'splash_screen.dart';
-export 'login_screen.dart';
-export 'onboarding_screen.dart';
 export 'home_screen.dart';
-export 'sessions_screen.dart';
+export 'login_screen.dart';
+export 'map_screen.dart';
+export 'onboarding_screen.dart';
+export 'profile_screen.dart';
 export 'retraps_screen.dart';
 export 'search_screen.dart';
-export 'map_screen.dart';
-export 'profile_screen.dart';
+export 'sessions_screen.dart';
+export 'splash_screen.dart';
