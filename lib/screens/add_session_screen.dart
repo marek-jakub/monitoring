299469a2bdba283f8ed0e479f088c0f150c2f415
marek-jakub <mj3935538@gmail.com>
2023-05-31 15:46:21 +0200

    Adding autocomplete data import.

--- lib/screens/add_session_screen.dart
+++ lib/screens/add_session_screen.dart
@@ -4,6 +4,7 @@ import 'package:drift/drift.dart' as d;
 import 'package:geolocator/geolocator.dart';
 
 import '../data/monitoring_db.dart';
+import '../data/data_for_autocomplete/autocomplete_data.dart';
 import '../models/models.dart';
 
 import '../widgets/custom_text_form_field.dart';
