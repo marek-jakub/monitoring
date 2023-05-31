fe3a76b158eef293a21b6048e670d454f0cbf56e
marek-jakub <mj3935538@gmail.com>
2023-05-31 15:18:12 +0200

    Adding text form field.

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,1 +1,48 @@
-// TODO: create text form field
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField(
+      {Key? key,
+      required TextEditingController controller,
+      required String txtLabel,
+      required String keyboard})
+      : _controller = controller,
+        _label = txtLabel,
+        _keyboard = keyboard,
+        super(key: key);
+
+  final TextEditingController _controller;
+  final String _label;
+  final String _keyboard;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: TextFormField(
+        controller: widget._controller,
+        keyboardType: widget._keyboard == 'text'
+            ? TextInputType.text
+            : TextInputType.number,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          label: Text(widget._label),
+          fillColor: Colors.white,
+          filled: true,
+        ),
+        style: const TextStyle(fontSize: 14),
+        validator: (value) {
+          // if (value == null || value.isEmpty) {
+          //   return '${widget._label} cannot be empty!';
+          // }
+          return null;
+        },
+      ),
+    );
+  }
+}
