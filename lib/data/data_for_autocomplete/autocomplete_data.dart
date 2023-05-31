2eece746c692322e287518dcbba000f9651892d5
marek-jakub <mj3935538@gmail.com>
2023-05-31 15:45:59 +0200

    Adding place code data.

--- lib/data/data_for_autocomplete/autocomplete_data.dart
+++ lib/data/data_for_autocomplete/autocomplete_data.dart
@@ -1,1 +1,28 @@
-// TODO: create lists for autocomplete fields.
+/// A list holding EURING Code values of place code.
+///
+/// Currently not complete.
+const List<String> placeCode = <String>[
+  '',
+  'GB--',
+  'GBMB',
+  'GBMK',
+  'GBML',
+  'GBMN',
+  'GBMO',
+  'GBMT',
+  'GBMW',
+  'GBNA',
+  'GBNE',
+  'GBNG',
+  'GBNO',
+  'GBNP',
+  'GBNR',
+  'GBNS',
+  'GBPB',
+  'GBPE',
+  'GBPH',
+  'GBPL',
+  'GBPM',
+  'GBPP',
+  'GBL',
+];
