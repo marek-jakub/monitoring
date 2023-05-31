5d60deb68f03fd73740a935b26a6dab03106717b
marek-jakub <mj3935538@gmail.com>
2023-05-31 16:12:15 +0200

    Creating custom time picker.

--- lib/widgets/custom_time_picker_field.dart
+++ lib/widgets/custom_time_picker_field.dart
@@ -1,1 +1,45 @@
-// TODO: create custom time picker.
+import 'package:flutter/material.dart';
+
+class CustomTimePickerField extends StatefulWidget {
+  const CustomTimePickerField(
+      {Key? key,
+      required TextEditingController controller,
+      required String txtLabel,
+      required VoidCallback callback})
+      : _controller = controller,
+        _label = txtLabel,
+        _callback = callback,
+        super(key: key);
+
+  final TextEditingController _controller;
+  final String _label;
+  final VoidCallback _callback;
+
+  @override
+  State<CustomTimePickerField> createState() => _CustomTimePickerFieldState();
+}
+
+class _CustomTimePickerFieldState extends State<CustomTimePickerField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: TextFormField(
+        controller: widget._controller,
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          label: Text(widget._label),
+        ),
+        style: const TextStyle(fontSize: 14),
+        validator: (value) {
+          // if (value == null || value.isEmpty) {
+          //   return '${widget._label} cannot be empty!';
+          // }
+          return null;
+        },
+        onTap: widget._callback,
+      ),
+    );
+  }
+}
