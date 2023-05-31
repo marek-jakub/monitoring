7975aa2d3538a74e19d6390a05d0b6be91699b7c
marek-jakub <mj3935538@gmail.com>
2023-05-31 15:50:04 +0200

    Adding accuracy of date.

--- lib/data/data_for_autocomplete/autocomplete_data.dart
+++ lib/data/data_for_autocomplete/autocomplete_data.dart
@@ -1,3 +1,18 @@
+/// A list holding EURING Code accuracy of date values.
+const List<String> accuracyOfDate = <String>[
+  '',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+];
+
 /// A list holding EURING Code values of place code.
 ///
 /// Currently not complete.
