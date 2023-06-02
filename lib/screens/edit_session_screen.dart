2ba67810eb88066c2b8df38f198c40b7415cb726
marek-jakub <mj3935538@gmail.com>
2023-06-02 16:45:32 +0200

    Adding text controllers and listeners.

--- lib/screens/edit_session_screen.dart
+++ lib/screens/edit_session_screen.dart
@@ -30,7 +30,142 @@ class EditSessionScreen extends StatefulWidget {
 }
 
 class _EditSessionScreenState extends State<EditSessionScreen> {
+  /// Form key for accessing and checking form attributes' state.
+  final _sessionFormKey = GlobalKey<FormState>();
+
+  /// Focus to be able to distinguish between start and end time fields.
+  bool _startTimeFieldFocus = false;
+
+  // Session information controllers
+  //  final TextEditingController _ringingSchemeController =
+  //    TextEditingController();
+  final TextEditingController _ringerId = TextEditingController();
+  final TextEditingController _placeCodeController = TextEditingController();
+  final TextEditingController _localityController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _accuracyOfDateController =
+      TextEditingController();
+  final TextEditingController _latController = TextEditingController();
+  final TextEditingController _lonController = TextEditingController();
+  final TextEditingController _coordAccuracyController =
+      TextEditingController();
+  final TextEditingController _startTimeController = TextEditingController();
+  final TextEditingController _endTimeController = TextEditingController();
+  final TextEditingController _localeInfoController = TextEditingController();
+
+  /// Current date.
+  DateTime? _currDate;
+
+  /// Current time.
+  TimeOfDay? _currTime;
+
+  /// A state of the GPS sensor service.
+  bool serviceStatus = false;
+
+  /// User permission to access GPS sensor service.
+  bool hasPermission = false;
+
+  /// Permission to access user's location services.
+  late LocationPermission permission;
+
+  /// User's geographical position.
+  late Position position;
+
+  /// Provider and notifier access to data manager.
+  late RingDataManager _dataManager;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _dataManager = Provider.of<RingDataManager>(context, listen: false);
+    _dataManager.addListener(providerListener);
+  }
+
+  @override
+  void dispose() {
+    // Dispose off session controllers
+    _ringerId.dispose();
+    _placeCodeController.dispose();
+    _localityController.dispose();
+    _dateController.dispose();
+    _accuracyOfDateController.dispose();
+    _latController.dispose();
+    _lonController.dispose();
+    _coordAccuracyController.dispose();
+    _startTimeController.dispose();
+    _endTimeController.dispose();
+    _localeInfoController.dispose();
+
+    // Remove session notifier listeners
+    _dataManager.removeListener(providerListener);
+
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold();
+  }
+
+  /// Listens to change notifier save session success or error.
+  void providerListener() {
+    if (_dataManager.isSessionAdded) {
+      listenAddSession();
+      context.read<RingDataManager>().setIsSessionAdded(false);
+    }
+
+    if (_dataManager.error != '') {
+      listenAddSessionError(_dataManager.error);
+      context.read<RingDataManager>().setError('');
+    }
+  }
+
+  /// Shows scaffold messenger on successfuly saved session data.
+  void listenAddSession() {
+    ScaffoldMessenger.of(context).showMaterialBanner(
+      MaterialBanner(
+        content: const Text(
+          'Session data updated',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.brown,
+        actions: [
+          TextButton(
+            onPressed: () {
+              context.read<RingDataManager>().setIsSessionAdded(false);
+              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+            },
+            child: const Text(
+              'Close',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Shows scaffold messenger with error on saving error.
+  void listenAddSessionError(String errorMsg) {
+    ScaffoldMessenger.of(context).showMaterialBanner(
+      MaterialBanner(
+        content: Text(
+          errorMsg,
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.brown,
+        actions: [
+          TextButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+            },
+            child: const Text(
+              'Close',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
