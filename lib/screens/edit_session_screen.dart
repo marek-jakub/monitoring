4b798d627e64d202e3b9e352314ff2a70b7d3494
marek-jakub <mj3935538@gmail.com>
2023-06-02 16:38:13 +0200

    Initial commit.

--- lib/screens/edit_session_screen.dart
+++ lib/screens/edit_session_screen.dart
@@ -12,3 +12,25 @@ import '../widgets/custom_text_form_field.dart';
 import '../widgets/custom_dropdown_form_field.dart';
 import '../widgets/custom_date_picker_field.dart';
 import '../widgets/custom_time_picker_field.dart';
+
+/// Screen used to edit session-location information.
+class EditSessionScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.editSessionPath,
+      key: ValueKey(MonitoRingPages.editSessionPath),
+      child: const EditSessionScreen(),
+    );
+  }
+
+  const EditSessionScreen({super.key});
+
+  @override
+  State<EditSessionScreen> createState() => _EditSessionScreenState();
+}
+
+class _EditSessionScreenState extends State<EditSessionScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
