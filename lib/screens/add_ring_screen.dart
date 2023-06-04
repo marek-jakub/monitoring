49d773a8a84516d00c35e27da22aeb73eddcd21d
marek-jakub <mj3935538@gmail.com>
2023-06-04 16:51:17 +0200

    Initial commit.

--- lib/screens/add_ring_screen.dart
+++ lib/screens/add_ring_screen.dart
@@ -12,3 +12,26 @@ import '../widgets/custom_text_form_field.dart';
 import '../widgets/custom_dropdown_form_field.dart';
 import '../widgets/custom_date_picker_field.dart';
 import '../widgets/custom_time_picker_field.dart';
+
+/// A form to input and save bird information as a ring entity.
+class AddRingScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.addRingPath,
+      key: ValueKey(MonitoRingPages.addRingPath),
+      child: const AddRingScreen(),
+    );
+  }
+
+  const AddRingScreen({super.key});
+
+  @override
+  State<AddRingScreen> createState() => _AddRingScreenState();
+}
+
+class _AddRingScreenState extends State<AddRingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
