17a16175f9ef0b204e4cb3d9c3161e84d1c28507
marek-jakub <mj3935538@gmail.com>
2023-06-05 09:46:08 +0200

    Adding age values list.

--- lib/data/data_for_autocomplete/autocomplete_data.dart
+++ lib/data/data_for_autocomplete/autocomplete_data.dart
@@ -59,6 +59,29 @@ const List<String> accuracyOfDate = <String>[
   '9',
 ];
 
+/// A list holding EURING code age values.
+const List<String> age = <String>[
+  '',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+];
+
 /// A list holding EURING Code metal ring information values.
 const List<String> metalRingInfo = <String>[
   '',
