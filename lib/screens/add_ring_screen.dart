cd8c6c95bcf1e9bc4f785127604a8224eb900230
marek-jakub <mj3935538@gmail.com>
2023-06-05 10:58:42 +0200

    Removing unused imports.

--- lib/screens/add_ring_screen.dart
+++ lib/screens/add_ring_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:drift/drift.dart' as d;
-import 'package:geolocator/geolocator.dart';
-import 'package:intl/intl.dart';
 
 import '../data/monitoring_db.dart';
 import '../data/data_for_autocomplete/autocomplete_data.dart';
@@ -10,7 +8,6 @@ import '../models/models.dart';
 
 import '../widgets/custom_text_form_field.dart';
 import '../widgets/custom_dropdown_form_field.dart';
-import '../widgets/custom_date_picker_field.dart';
 import '../widgets/custom_time_picker_field.dart';
 import '../widgets/custom_typeahead_field.dart';
 
