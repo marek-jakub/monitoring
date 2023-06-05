81236a2a9ab493535ae9e328fb8dc715bc86f864
marek-jakub <mj3935538@gmail.com>
2023-06-05 11:23:04 +0200

    Adding form keys for checking attributes' state.

--- lib/screens/edit_ring_screen.dart
+++ lib/screens/edit_ring_screen.dart
@@ -28,6 +28,15 @@ class EditRingScreen extends StatefulWidget {
 }
 
 class _EditRingScreenState extends State<EditRingScreen> {
+  /// Form key for accessing, checking required data.
+  final _requiredDataFormKey = GlobalKey<FormState>();
+
+  /// Form key for accessing, checking bird's body data values.
+  final _bodyMeasurementsFormKey = GlobalKey<FormState>();
+
+  /// Form key for accessing, checking circumstances information.
+  final _circumstancesFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
