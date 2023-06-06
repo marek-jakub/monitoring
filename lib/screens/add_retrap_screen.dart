27b92973dd4667ea7a33efba831a73674221ddd7
marek-jakub <mj3935538@gmail.com>
2023-06-06 16:51:44 +0200

    Adding text controllers.

--- lib/screens/add_retrap_screen.dart
+++ lib/screens/add_retrap_screen.dart
@@ -37,6 +37,67 @@ class _AddRetrapScreenState extends State<AddRetrapScreen> {
   /// Form key for accessing, checking circumstances information.
   final _circumstancesFormKey = GlobalKey<FormState>();
 
+  // Required identification Data controllers
+  final TextEditingController _ringSchemeController = TextEditingController();
+  final TextEditingController _ringSeriesCodeController =
+      TextEditingController();
+  final TextEditingController _ringIdNumberController = TextEditingController();
+  final TextEditingController _conditionController = TextEditingController();
+
+  // Information not required - circumstances
+  final TextEditingController _primaryIDMethodController =
+      TextEditingController();
+  final TextEditingController _metalRingInfoController =
+      TextEditingController();
+  final TextEditingController _speciesController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _sexController = TextEditingController();
+
+  final TextEditingController _sexingMethodController = TextEditingController();
+  final TextEditingController _catchingMethodController =
+      TextEditingController();
+  final TextEditingController _catchingLuresController =
+      TextEditingController();
+
+  final TextEditingController _statusController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  final TextEditingController _circumstancesController =
+      TextEditingController();
+  final TextEditingController _circumstancesPresumedController =
+      TextEditingController();
+  final TextEditingController _otherMarksController = TextEditingController();
+
+  // Information not required - body measurements
+  final TextEditingController _wingLengthController = TextEditingController();
+  final TextEditingController _thirdPrimaryController = TextEditingController();
+  final TextEditingController _wingPointStateController =
+      TextEditingController();
+  final TextEditingController _massController = TextEditingController();
+  final TextEditingController _moultController = TextEditingController();
+  final TextEditingController _plumageCodeController = TextEditingController();
+  final TextEditingController _hindClawController = TextEditingController();
+  final TextEditingController _billLengthController = TextEditingController();
+  final TextEditingController _billMethodController = TextEditingController();
+  final TextEditingController _headLengthTotalController =
+      TextEditingController();
+  final TextEditingController _tarsusController = TextEditingController();
+  final TextEditingController _tarsusMethodController = TextEditingController();
+  final TextEditingController _tailLengthController = TextEditingController();
+  final TextEditingController _tailDifferenceController =
+      TextEditingController();
+  final TextEditingController _fatScoreController = TextEditingController();
+  final TextEditingController _fatScoreMethodController =
+      TextEditingController();
+  final TextEditingController _pectoralMuscleController =
+      TextEditingController();
+  final TextEditingController _broodPatchController = TextEditingController();
+  final TextEditingController _primaryScoreController = TextEditingController();
+  final TextEditingController _primaryMoultController = TextEditingController();
+  final TextEditingController _oldGreaterCovertsController =
+      TextEditingController();
+  final TextEditingController _alulaController = TextEditingController();
+  final TextEditingController _carpalCovertController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
