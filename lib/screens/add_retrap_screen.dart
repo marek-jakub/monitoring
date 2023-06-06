8f59ada2fb58b0bfa362d2dec05260129fbbad93
marek-jakub <mj3935538@gmail.com>
2023-06-06 16:48:27 +0200

    Initial class commit.

--- lib/screens/add_retrap_screen.dart
+++ lib/screens/add_retrap_screen.dart
@@ -10,3 +10,26 @@ import '../widgets/custom_text_form_field.dart';
 import '../widgets/custom_dropdown_form_field.dart';
 import '../widgets/custom_time_picker_field.dart';
 import '../widgets/custom_typeahead_field.dart';
+
+/// A form to input and save recaptured bird information as a retrap entity.
+class AddRetrapScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.addRetrapPath,
+      key: ValueKey(MonitoRingPages.addRetrapPath),
+      child: const AddRetrapScreen(),
+    );
+  }
+
+  const AddRetrapScreen({super.key});
+
+  @override
+  State<AddRetrapScreen> createState() => _AddRetrapScreenState();
+}
+
+class _AddRetrapScreenState extends State<AddRetrapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
