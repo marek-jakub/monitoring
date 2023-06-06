93eb4e2270720ec6d1052e274efaf5d481acc628
marek-jakub <mj3935538@gmail.com>
2023-06-06 16:40:43 +0200

    Adding pick time widget.

--- lib/screens/edit_ring_screen.dart
+++ lib/screens/edit_ring_screen.dart
@@ -843,7 +843,7 @@ class _EditRingScreenState extends State<EditRingScreen> {
                             controller: _timeController,
                             txtLabel: 'Time',
                             callback: () {
-                              //pickTime(context);
+                              pickTime(context);
                             },
                           ),
                           CustomTextFormField(
@@ -1065,6 +1065,35 @@ class _EditRingScreenState extends State<EditRingScreen> {
         ],
       ),
     );
+  }
+
+  /// A time picker widget.
+  Future<void> pickTime(BuildContext context) async {
+    final initialTime = TimeOfDay.now();
+    final newTime = await showTimePicker(
+      context: context,
+      initialTime: _currTime ?? initialTime,
+      builder: (context, child) => Theme(
+        data: ThemeData().copyWith(
+          colorScheme: const ColorScheme.light(
+              primary: Colors.green,
+              onPrimary: Colors.white,
+              onSurface: Colors.black),
+          dialogBackgroundColor: Colors.white,
+        ),
+        child: child ?? const Text(''),
+      ),
+    );
+
+    if (newTime == null) {
+      return;
+    }
+
+    setState(() {
+      _currTime = newTime;
+      String time = _currTime!.format(context);
+      _timeController.text = time;
+    });
   }
 
   /// Populates form fields with selected ring information provided in
