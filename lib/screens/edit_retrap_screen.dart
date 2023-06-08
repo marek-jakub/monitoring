7de1db3501a69eab8039791068b8d266be6d206a
marek-jakub <mj3935538@gmail.com>
2023-06-08 11:10:56 +0200

    First class commit.

--- lib/screens/edit_retrap_screen.dart
+++ lib/screens/edit_retrap_screen.dart
@@ -10,3 +10,26 @@ import '../widgets/custom_text_form_field.dart';
 import '../widgets/custom_dropdown_form_field.dart';
 import '../widgets/custom_time_picker_field.dart';
 import '../widgets/custom_typeahead_field.dart';
+
+/// A form for editing, deleting retrap data stored in the database.
+class EditRetrapScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.editRetrapPath,
+      key: ValueKey(MonitoRingPages.editRetrapPath),
+      child: const EditRetrapScreen(),
+    );
+  }
+
+  const EditRetrapScreen({super.key});
+
+  @override
+  State<EditRetrapScreen> createState() => _EditRetrapScreenState();
+}
+
+class _EditRetrapScreenState extends State<EditRetrapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
