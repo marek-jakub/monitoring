c73400e08964fc19e5123bd4cfdddc2536052533
marek-jakub <mj3935538@gmail.com>
2023-06-11 13:42:57 +0200

    Add scaffold widget with text.

--- lib/screens/add_ring_series_screen.dart
+++ lib/screens/add_ring_series_screen.dart
@@ -2,3 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/models.dart';
+
+/// A form to input and save ring series information as a ringseries entity.
+class AddRingSeriesScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.addRingSeriesPath,
+      key: ValueKey(MonitoRingPages.addRingSeriesPath),
+      child: const AddRingSeriesScreen(),
+    );
+  }
+
+  const AddRingSeriesScreen({super.key});
+
+  @override
+  State<AddRingSeriesScreen> createState() => _AddRingSeriesScreenState();
+}
+
+class _AddRingSeriesScreenState extends State<AddRingSeriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'MonitoRing: Ring Series',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const BackButtonIcon(),
+          onPressed: () {
+            Provider.of<RingDataManager>(context, listen: false)
+                .setNewRingSeries(false);
+          },
+        ),
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('ring_series_screen'),
+        ],
+      ),
+    );
+  }
+}
