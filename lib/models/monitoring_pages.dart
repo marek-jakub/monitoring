6a2b4d7954c71a3120b8adabd47906f9fdf56cc7
marek-jakub <mj3935538@gmail.com>
2023-06-13 18:13:41 +0200

    Change code placements.

--- lib/models/monitoring_pages.dart
+++ lib/models/monitoring_pages.dart
@@ -4,19 +4,20 @@ class MonitoRingPages {
   static String loginPath = '/login';
   static String onboardingPath = '/onboarding';
   static String home = '/';
+  static String sessionsPath = '/sessions';
+  static String retrapsPath = '/retraps';
+  static String searchPath = '/search';
+  static String mapPath = '/map';
   static String addSessionPath = '/addSession';
   static String editSessionPath = '/editSession';
   static String addRingPath = '/addRing';
   static String editRingPath = '/editRing';
   static String addRetrapPath = '/addRetrap';
   static String editRetrapPath = '/editRetrap';
+  static String reportPath = '/report';
   static String profilePath = '/profile';
   static String euringCodePath = '/euring';
   static String addRingSeriesPath = '/addSeries';
   static String addLostRingPath = '/addLostRing';
   static String orderRingsPath = '/orderRings';
-  static String sessionsPath = '/sessions';
-  static String retrapsPath = '/retraps';
-  static String searchPath = '/search';
-  static String mapPath = '/map';
 }
