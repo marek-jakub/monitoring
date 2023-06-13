736ec9f0a3336134ed14124a65dc033a3b2d228c
marek-jakub <mj3935538@gmail.com>
2023-06-13 18:20:24 +0200

    Build widget first commit.

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -2,3 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/models.dart';
+
+/// A screen for data output as an EURING compatible .csv file.
+class ReportScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.reportPath,
+      key: ValueKey(MonitoRingPages.reportPath),
+      child: const ReportScreen(),
+    );
+  }
+
+  const ReportScreen({super.key});
+
+  @override
+  State<ReportScreen> createState() => _ReportScreenState();
+}
+
+class _ReportScreenState extends State<ReportScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'MonitoRing: report',
+        ),
+        leading: IconButton(
+          icon: const BackButtonIcon(),
+          onPressed: () {
+            Provider.of<RingDataManager>(context, listen: false)
+                .setNewReport(false);
+          },
+        ),
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('report_screen'),
+        ],
+      ),
+    );
+  }
+}
