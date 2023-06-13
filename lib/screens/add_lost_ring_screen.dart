ed85d46d9e8e8646e4d33bece0edc738543f3af4
marek-jakub <mj3935538@gmail.com>
2023-06-13 17:52:51 +0200

    First build widget commit.

--- lib/screens/add_lost_ring_screen.dart
+++ lib/screens/add_lost_ring_screen.dart
@@ -2,3 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/models.dart';
+
+/// A form to input and save lost ring information as a lost ring entity.
+class LostRingsScreen extends StatefulWidget {
+  static MaterialPage page() {
+    return MaterialPage(
+      name: MonitoRingPages.addLostRingPath,
+      key: ValueKey(MonitoRingPages.addLostRingPath),
+      child: const LostRingsScreen(),
+    );
+  }
+
+  const LostRingsScreen({super.key});
+
+  @override
+  State<LostRingsScreen> createState() => _LostRingsScreenState();
+}
+
+class _LostRingsScreenState extends State<LostRingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'MonitoRing: lost rings',
+        ),
+        leading: IconButton(
+          icon: const BackButtonIcon(),
+          onPressed: () {
+            Provider.of<RingDataManager>(context, listen: false)
+                .setNewLostRing(false);
+          },
+        ),
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('lost_rings_screen'),
+        ],
+      ),
+    );
+  }
+}
