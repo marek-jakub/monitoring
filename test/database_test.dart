5ffc8e90175d1326d0f234928ebfb5ebcfaa8a3e
marek-jakub <mj3935538@gmail.com>
2023-06-14 18:10:19 +0200

    Removing test data, adding import of test data.

--- test/database_test.dart
+++ test/database_test.dart
@@ -1,27 +1,9 @@
 import 'package:drift/native.dart';
 import 'package:test/test.dart';
-import 'package:drift/drift.dart' as d;
 
 import 'package:monitoring/data/monitoring_db.dart';
 
-List<SessionEntityCompanion> sessions = [
-  const SessionEntityCompanion(
-    date: d.Value('06-06-2023'),
-    dateAccuracy: d.Value('0'),
-    location: d.Value(1),
-    ringerId: d.Value('6000'),
-    startTime: d.Value('04:00'),
-    endTime: d.Value('10:00'),
-  ),
-  const SessionEntityCompanion(
-    date: d.Value('07-06-2023'),
-    dateAccuracy: d.Value('1'),
-    location: d.Value(2),
-    ringerId: d.Value('7000'),
-    startTime: d.Value('05:00'),
-    endTime: d.Value('11:00'),
-  ),
-];
+import 'database_test_data.dart';
 
 void main() {
   late MonRingDb monRingDb;
