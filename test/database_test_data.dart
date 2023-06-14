35f75c375ea3b8692f80e7988d4708f2cf0322e2
marek-jakub <mj3935538@gmail.com>
2023-06-14 18:11:51 +0200

    Add session entity test data.

--- test/database_test_data.dart
+++ test/database_test_data.dart
@@ -1,1 +1,22 @@
-// TODO: create entity companions to serve as test data.
+import 'package:drift/drift.dart' as d;
+import 'package:monitoring/data/monitoring_db.dart';
+
+/// Test data for database in the form of table entity companions.
+List<SessionEntityCompanion> sessions = [
+  const SessionEntityCompanion(
+    date: d.Value('06-06-2023'),
+    dateAccuracy: d.Value('0'),
+    location: d.Value(1),
+    ringerId: d.Value('6000'),
+    startTime: d.Value('04:00'),
+    endTime: d.Value('10:00'),
+  ),
+  const SessionEntityCompanion(
+    date: d.Value('07-06-2023'),
+    dateAccuracy: d.Value('1'),
+    location: d.Value(2),
+    ringerId: d.Value('7000'),
+    startTime: d.Value('05:00'),
+    endTime: d.Value('11:00'),
+  ),
+];
