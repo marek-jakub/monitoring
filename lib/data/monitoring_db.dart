e533c3d42aaab4b478a98b918247589d3c165142
marek-jakub <marekjakub@hotmail.co.uk>
2023-07-19 20:40:57 +0200

    Change database file name.

--- lib/data/monitoring_db.dart
+++ lib/data/monitoring_db.dart
@@ -192,7 +192,7 @@ class MonRingDb extends _$MonRingDb {
 LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'db_a.sqlite'));
+    final file = File(p.join(dbFolder.path, 'db_b.sqlite'));
 
     //return NativeDatabase.createInBackground(file);
     return NativeDatabase(file);
