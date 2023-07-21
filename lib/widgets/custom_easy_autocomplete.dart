ff4133f7e2292fe6aadc99d8a49b709cfd750c32
marek-jakub <marekjakub@hotmail.co.uk>
2023-07-21 18:06:32 +0200

    Create easy autocomplete.

--- lib/widgets/custom_easy_autocomplete.dart
+++ lib/widgets/custom_easy_autocomplete.dart
@@ -1,1 +1,53 @@
-// TODO: create autocomplete form field.
+import 'package:flutter/material.dart';
+import 'package:easy_autocomplete/easy_autocomplete.dart';
+
+class CustomEasyAutocomplete extends StatefulWidget {
+  const CustomEasyAutocomplete({
+    Key? key,
+    required TextEditingController controller,
+    required String txtLabel,
+    required List<String> listValues,
+  })  : _controller = controller,
+        _label = txtLabel,
+        _listValues = listValues,
+        super(key: key);
+
+  final TextEditingController _controller;
+  final String _label;
+  final List<String> _listValues;
+
+  @override
+  State<CustomEasyAutocomplete> createState() => _CustomEasyAutocompleteState();
+}
+
+class _CustomEasyAutocompleteState extends State<CustomEasyAutocomplete> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: EasyAutocomplete(
+        decoration: InputDecoration(
+          labelText: widget._label,
+          border: const OutlineInputBorder(),
+        ),
+        suggestions: widget._listValues,
+        suggestionBuilder: (data) {
+          return Container(
+            margin: const EdgeInsets.all(1),
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              data,
+              style: const TextStyle(color: Colors.white),
+            ),
+          );
+        },
+        onChanged: (value) => widget._controller.text = value,
+        validator: (species) => species == null ? 'Invalid species' : null,
+      ),
+    );
+  }
+}
