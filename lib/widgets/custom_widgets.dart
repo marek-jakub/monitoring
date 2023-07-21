cab05d15488e185300f8bf25a5e38d7a7439854d
marek-jakub <marekjakub@hotmail.co.uk>
2023-07-21 18:09:02 +0200

    Add easy autocomplete export.

--- lib/widgets/custom_widgets.dart
+++ lib/widgets/custom_widgets.dart
@@ -2,6 +2,7 @@ export 'custom_autocomplete_formfield.dart';
 export 'custom_date_picker_field.dart';
 export 'custom_dropdown_country_field.dart';
 export 'custom_dropdown_form_field.dart';
+export 'custom_easy_autocomplete.dart';
 export 'custom_text_form_field.dart';
 export 'custom_time_picker_field.dart';
 export 'custom_typeahead_field.dart';
