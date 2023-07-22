84a606b631c6294e15282bb2342b452215206b57
marek-jakub <marekjakub@hotmail.co.uk>
2023-07-22 20:33:54 +0200

    Add output of data.

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:monitoring/data/monitoring_db.dart';
 
 import '../models/models.dart';
 
@@ -20,28 +25,56 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
+  List<RingEntityData> rings = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'MonitoRing: report',
-        ),
-        leading: IconButton(
-          icon: const BackButtonIcon(),
-          onPressed: () {
-            Provider.of<RingDataManager>(context, listen: false)
-                .setNewReport(false);
-          },
-        ),
-      ),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text('report_screen'),
-        ],
-      ),
+    return Consumer<RingDataManager>(
+      builder: (context, dataManager, child) {
+        dataManager.fetchAllRings();
+        rings = dataManager.allRings;
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'MonitoRing: report',
+            ),
+            leading: IconButton(
+              icon: const BackButtonIcon(),
+              onPressed: () {
+                Provider.of<RingDataManager>(context, listen: false)
+                    .setNewReport(false);
+              },
+            ),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  String jsonRings = jsonEncode(rings);
+                  _saveJsonAsFile(jsonRings);
+                },
+                child: const Text('Order rings'),
+              ),
+            ],
+          ),
+        );
+      },
     );
+  }
+
+  _saveJsonAsFile(String json) async {
+    File ringsFile = File(await _getFilePath());
+    ringsFile.writeAsString(json);
+    debugPrint(ringsFile.toString());
+  }
+
+  Future<String> _getFilePath() async {
+    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
+    String appDocumentsPath = appDocumentsDirectory.path;
+    String filePath = '$appDocumentsPath/jsonRings';
+
+    return filePath;
   }
 }
