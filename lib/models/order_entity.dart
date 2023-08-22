7f0f9028c7a0e33c0f47b4f2e999b4f6ff082286
marek-jakub <marekjakub@hotmail.co.uk>
2023-08-22 14:52:45 +0200

    Add ringId field.

--- lib/models/order_entity.dart
+++ lib/models/order_entity.dart
@@ -4,6 +4,7 @@ import 'package:drift/drift.dart';
 class OrderEntity extends Table {
   // non-nullable
   IntColumn get id => integer().autoIncrement()();
+  TextColumn get ringerId => text()();
   TextColumn get ringSeriesCode => text()();
   IntColumn get amount => integer()();
 }
