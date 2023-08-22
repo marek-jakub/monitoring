b53af7ba566494d13c693b40d4531c31110321fa
marek-jakub <marekjakub@hotmail.co.uk>
2023-08-22 14:53:28 +0200

    Add ringerId field.

--- lib/models/used_ring_entity.dart
+++ lib/models/used_ring_entity.dart
@@ -4,6 +4,7 @@ import 'package:drift/drift.dart';
 class UsedRingEntity extends Table {
   // non-nullable
   IntColumn get id => integer().autoIncrement()();
+  TextColumn get ringerId => text()();
   TextColumn get ringSeriesCode => text()();
   TextColumn get idNumber => text()();
 }
