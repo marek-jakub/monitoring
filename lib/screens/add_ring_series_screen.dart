011557227a8ccc61665e26531de7dc119c8b7b64
marek-jakub <marekjakub@hotmail.co.uk>
2023-08-27 15:59:08 +0200

    Add addRingSeries method.

--- lib/screens/add_ring_series_screen.dart
+++ lib/screens/add_ring_series_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:monitoring/data/data_for_autocomplete/autocomplete_data.dart';
 import 'package:monitoring/data/monitoring_db.dart';
 import 'package:provider/provider.dart';
+import 'package:drift/drift.dart' as d;
 
 import '../models/models.dart';
 
@@ -41,7 +42,7 @@ class _AddRingSeriesScreenState extends State<AddRingSeriesScreen> {
     super.initState();
 
     _dataManager = Provider.of<RingDataManager>(context, listen: false);
-    // _dataManager.addListener(providerListener);
+    //_dataManager.addListener(providerListener);
   }
 
   @override
@@ -61,7 +62,7 @@ class _AddRingSeriesScreenState extends State<AddRingSeriesScreen> {
   @override
   Widget build(BuildContext context) {
     return Consumer<RingDataManager>(
-      builder: (context, ringDataManager, child) {
+      builder: (context, dataManager, child) {
         return Scaffold(
           appBar: AppBar(
             title: const Text(
@@ -109,7 +110,13 @@ class _AddRingSeriesScreenState extends State<AddRingSeriesScreen> {
                           padding:
                               const EdgeInsets.fromLTRB(5.0, 2.0, 2.0, 2.0),
                           child: ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              addRingSeries();
+                              _dataManager.getRingSeriesStream(context
+                                  .read<ProfileManager>()
+                                  .getRinger
+                                  .ringerId);
+                            },
                             child: const Text('Add'),
                           ),
                         ),
@@ -138,79 +145,100 @@ class _AddRingSeriesScreenState extends State<AddRingSeriesScreen> {
                 height: 10,
               ),
               const Text('Available ring series'),
-              // Selector<RingDataManager, List<RingseriesEntityData>>(
-              //   selector: (context, notifier) => notifier.ringSeriesStream,
-              //   builder: (context, ringSeries, child) {
-              //     return ListView.builder(
-              //         itemCount: ringSeries.length,
-              //         itemBuilder: (context, index) {
-              //           final ringseries = ringSeries[index];
-              //           return GestureDetector(
-              //             onTap: () {},
-              //             child: Card(
-              //               elevation: 0,
-              //               child: Column(
-              //                 children: [
-              //                   Row(
-              //                     children: [
-              //                       const Expanded(
-              //                         child: Text('Ring series code:',
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                       Expanded(
-              //                         child: Text(ringseries.code,
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                     ],
-              //                   ),
-              //                   Row(
-              //                     children: [
-              //                       const Expanded(
-              //                         child: Text('Scheme code:',
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                       Expanded(
-              //                         child: Text(ringseries.schemeCode,
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                     ],
-              //                   ),
-              //                   Row(
-              //                     children: [
-              //                       const Expanded(
-              //                         child: Text('Series from:',
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                       Expanded(
-              //                         child: Text(
-              //                             ringseries.ringfrom.toString(),
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                     ],
-              //                   ),
-              //                   Row(
-              //                     children: [
-              //                       const Expanded(
-              //                         child: Text('Series to:',
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                       Expanded(
-              //                         child: Text(ringseries.ringto.toString(),
-              //                             textAlign: TextAlign.left),
-              //                       ),
-              //                     ],
-              //                   ),
-              //                 ],
-              //               ),
-              //             ),
-              //           );
-              //         });
-              //   },
-              // ),
+              Expanded(
+                child: Selector<RingDataManager, List<RingseriesEntityData>>(
+                  selector: (context, notifier) => notifier.ringSeriesStream,
+                  builder: (context, ringSeries, child) {
+                    return ListView.builder(
+                        itemCount: ringSeries.length,
+                        itemBuilder: (context, index) {
+                          final ringseries = ringSeries[index];
+                          return GestureDetector(
+                            onTap: () {},
+                            child: Card(
+                              elevation: 0,
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      const Expanded(
+                                        child: Text('Ring series code:',
+                                            textAlign: TextAlign.left),
+                                      ),
+                                      Expanded(
+                                        child: Text(ringseries.code,
+                                            textAlign: TextAlign.left),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Expanded(
+                                        child: Text('Scheme code:',
+                                            textAlign: TextAlign.left),
+                                      ),
+                                      Expanded(
+                                        child: Text(ringseries.schemeCode,
+                                            textAlign: TextAlign.left),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Expanded(
+                                        child: Text('Series from:',
+                                            textAlign: TextAlign.left),
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                            ringseries.ringfrom.toString(),
+                                            textAlign: TextAlign.left),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Expanded(
+                                        child: Text('Series to:',
+                                            textAlign: TextAlign.left),
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                            ringseries.ringto.toString(),
+                                            textAlign: TextAlign.left),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  },
+                ),
+              ),
             ],
           ),
         );
       },
     );
+  }
+
+  /// Saves ring series information in the database.
+  void addRingSeries() {
+    /// Checks for input validity
+    final isValid = _ringSeriesFormKey.currentState?.validate();
+
+    if (isValid != null && isValid) {
+      final ringSeriesEntity = RingseriesEntityCompanion(
+        ringerId: d.Value(context.read<ProfileManager>().getRinger.ringerId),
+        code: d.Value(_seriesCode.text),
+        schemeCode: d.Value(_schemeCode.text),
+        ringfrom: d.Value(int.parse(_ringFrom.text)),
+        ringto: d.Value(int.parse(_ringTo.text)),
+      );
+
+      context.read<RingDataManager>().saveRingSeries(ringSeriesEntity);
+    }
   }
 }
