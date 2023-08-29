bcfa8cdec2a717f23948fc8e01112ac18fc9ac1d
marek-jakub <marekjakub@hotmail.co.uk>
2023-08-29 17:53:17 +0200

    Add text editing controllers.

--- lib/screens/add_lost_ring_screen.dart
+++ lib/screens/add_lost_ring_screen.dart
@@ -25,6 +25,10 @@ class _LostRingsScreenState extends State<LostRingsScreen> {
   /// Form key for accessing and checking form attributes' state.
   final _lostRingFormKey = GlobalKey<FormState>();
 
+  // Ring series information controllers
+  final TextEditingController _ringSeriesCode = TextEditingController();
+  final TextEditingController _ringId = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
