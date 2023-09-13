eac6654a10b92351c86673a41c433465680a9830
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-13 14:57:22 +0200

    Add place code validator.

--- lib/data/data_for_autocomplete/data_validation.dart
+++ lib/data/data_for_autocomplete/data_validation.dart
@@ -1,1 +1,15 @@
-// TODO: Implement data validation
+import 'autocomplete_data.dart';
+
+class InputValidator {
+  // Session input validation
+
+  // Place code
+  String? Function(String) placeCodeValidator = (String placeCode) {
+    if (placeCode.isEmpty) {
+      return 'Place code should not be empty';
+    } else if (!placeCodes.containsValue(placeCode)) {
+      return 'Unrecognized place code';
+    }
+    return null;
+  };
+}
