fc05449a1be2bde08375746ed5e374dbdbf670f8
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-20 16:39:11 +0200

    Initial commit.

---
+++ lib/widgets/custom_sex_checkbox_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class SexCheckboxFormField extends FormField<int> {
+  SexCheckboxFormField({
+    super.key,
+    TextEditingController? controller,
+    FormFieldValidator<int>? validator,
+    bool sexSelected = false,
+  }) : super(
+          validator: validator,
+          builder: (FormFieldState<int> state) {
+            return Row(
+              children: [
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text(
+                      'F',
+                    ),
+                    value: controller?.text == 'F' ? true : false,
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (value) {
+                      controller?.text = value! ? 'F' : '';
+                      sexSelected = value! ? true : false;
+                      state.didChange(value ? 1 : 0);
+                    },
+                    tileColor: state.hasError ? Colors.red : null,
+                  ),
+                ),
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text(
+                      'M',
+                    ),
+                    value: controller?.text == 'M' ? true : false,
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (value) {
+                      controller?.text = value! ? 'M' : '';
+                      sexSelected = value! ? true : false;
+                      state.didChange(value ? 2 : 0);
+                    },
+                    tileColor: state.hasError ? Colors.red : null,
+                  ),
+                ),
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text(
+                      'U',
+                    ),
+                    value: controller?.text == 'U' ? true : false,
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (value) {
+                      controller?.text = value! ? 'U' : '';
+                      sexSelected = value! ? true : false;
+                      state.didChange(value ? 3 : 0);
+                    },
+                    tileColor: state.hasError ? Colors.red : null,
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+}
