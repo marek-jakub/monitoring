387edd7804ebdb9131dbf6f452276ce98da0bd62
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-23 17:15:49 +0200

    Add file export.

--- lib/widgets/custom_widgets.dart
+++ lib/widgets/custom_widgets.dart
@@ -2,6 +2,7 @@ export 'custom_autocomplete_formfield.dart';
 export 'custom_date_picker_field.dart';
 export 'custom_dropdown_country_field.dart';
 export 'custom_dropdown_form_field.dart';
+export 'custom_dropdown_ring_field.dart';
 export 'custom_dropdown_series_field.dart';
 export 'custom_easy_autocomplete.dart';
 export 'custom_sex_checkbox_field.dart';
