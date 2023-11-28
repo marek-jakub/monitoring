24bb472a907fa74634209abd40a17a16c53cd088
marek-jakub <marekjakub@hotmail.co.uk>
2023-11-28 18:24:28 +0100

    Update unit tests.

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -4,13 +4,13 @@ import 'package:drift/drift.dart' as d;
 
 import 'package:monitoring/data/monitoring_db.dart';
 
-import 'database_test_data.dart' as test_data;
+import 'database_mock_data.dart' as mock_data;
 
 void main() {
   late MonRingDb monRingDb;
 
   setUp(() {
-    // On testing uncomment below and comment out initializer with no paramenters.
+    // On testing uncomment below and comment out initializer with no parameters.
     monRingDb = MonRingDb(NativeDatabase.memory());
     //monRingDb = MonRingDb();
   });
@@ -21,7 +21,7 @@ void main() {
 
   group('Session data can be saved, updated, retrieved and deleted.', () {
     test('Session can be saved.', () async {
-      final id = await monRingDb.saveSession(test_data.sessions[0]);
+      final id = await monRingDb.saveSession(mock_data.sessions[0]);
       final session = await monRingDb.getSession(id);
 
       expect(session.id, id);
@@ -34,7 +34,7 @@ void main() {
     });
 
     test('Another session can be saved, updated and deleted.', () async {
-      final id = await monRingDb.saveSession(test_data.sessions[1]);
+      final id = await monRingDb.saveSession(mock_data.sessions[1]);
       final session = await monRingDb.getSession(id);
 
       expect(session.id, id);
@@ -50,8 +50,8 @@ void main() {
           id: d.Value(id),
           date: const d.Value('08-06-2023'),
           dateAccuracy: const d.Value('2'),
-          location: const d.Value(3),
-          ringerId: const d.Value('8000'),
+          location: const d.Value(2),
+          ringerId: const d.Value('7000'),
           startTime: const d.Value('06:00'),
           endTime: const d.Value('12:00'),
         ),
@@ -61,8 +61,8 @@ void main() {
       expect(updated, true);
       expect(updatedSession.date.toString(), '08-06-2023');
       expect(updatedSession.dateAccuracy.toString(), '2');
-      expect(updatedSession.location, 3);
-      expect(updatedSession.ringerId.toString(), '8000');
+      expect(updatedSession.location, 2);
+      expect(updatedSession.ringerId.toString(), '7000');
       expect(updatedSession.startTime.toString(), '06:00');
       expect(updatedSession.endTime.toString(), '12:00');
 
@@ -74,52 +74,58 @@ void main() {
 
   group('Location data can be saved, updated, retrieved and deleted.', () {
     test('Location can be saved.', () async {
-      final id = await monRingDb.saveLocation(test_data.locations[0]);
+      final id = await monRingDb.saveLocation(mock_data.locations[0]);
       final location = await monRingDb.getLocationById(id);
 
       expect(location.id, id);
-      expect(location.locality.toString(), 'London');
-      expect(location.placeCode.toString(), 'GB');
+      expect(location.ringerId.toString(), '6000');
+      expect(location.country.toString(), 'SLOVAKIA');
+      expect(location.locality.toString(), 'Great Meadow');
+      expect(location.placeCode.toString(), 'SKVT');
       expect(location.latitude.toString(), '49.00');
       expect(location.longitude.toString(), '0.012');
       expect(location.coordinatesAccuracy.toString(), '0');
-      expect(location.localeInfo.toString(), 'Nature reserve Thames');
+      expect(location.localeInfo.toString(), 'Nature reserve Great Meadow');
     });
 
     test('Another location can be saved, updated and deleted.', () async {
-      final id = await monRingDb.saveLocation(test_data.locations[1]);
+      final id = await monRingDb.saveLocation(mock_data.locations[1]);
       final location = await monRingDb.getLocationById(id);
 
       expect(location.id, id);
-      expect(location.locality.toString(), 'Paris');
-      expect(location.placeCode.toString(), 'FR');
+      expect(location.ringerId.toString(), '7000');
+      expect(location.country.toString(), 'UNITED KINGDOM');
+      expect(location.locality.toString(), 'Sandown');
+      expect(location.placeCode.toString(), 'GBL');
       expect(location.latitude.toString(), '41.00');
       expect(location.longitude.toString(), '7.012');
       expect(location.coordinatesAccuracy.toString(), '1');
-      expect(location.localeInfo.toString(), 'Blue Forest');
+      expect(location.localeInfo.toString(), 'Thames sanctuary');
 
       final updated = await monRingDb.updateLocation(
         LocationEntityCompanion(
           id: d.Value(id),
-          ringerId: const d.Value('6000'),
+          ringerId: const d.Value('7000'),
           country: const d.Value('SLOVAKIA'),
-          locality: const d.Value('Rome'),
-          placeCode: const d.Value('ITA'),
+          locality: const d.Value('Black Forest'),
+          placeCode: const d.Value('SKVT'),
           latitude: const d.Value('36.00'),
           longitude: const d.Value('19.25'),
           coordinatesAccuracy: const d.Value('2'),
-          localeInfo: const d.Value('Dulle terraces'),
+          localeInfo: const d.Value('Nature reserve'),
         ),
       );
       final updatedLocation = await monRingDb.getLocationById(id);
 
       expect(updated, true);
-      expect(updatedLocation.locality.toString(), 'Rome');
-      expect(updatedLocation.placeCode.toString(), 'ITA');
+      expect(updatedLocation.ringerId.toString(), '7000');
+      expect(updatedLocation.country.toString(), 'SLOVAKIA');
+      expect(updatedLocation.locality.toString(), 'Black Forest');
+      expect(updatedLocation.placeCode.toString(), 'SKVT');
       expect(updatedLocation.latitude, '36.00');
       expect(updatedLocation.longitude.toString(), '19.25');
       expect(updatedLocation.coordinatesAccuracy.toString(), '2');
-      expect(updatedLocation.localeInfo.toString(), 'Dulle terraces');
+      expect(updatedLocation.localeInfo.toString(), 'Nature reserve');
 
       final deleted = await monRingDb.deleteLocation(id);
 
@@ -129,16 +135,17 @@ void main() {
 
   group('Ring data can be saved, updated, retrieved and deleted.', () {
     test('Ring data can be saved.', () async {
-      final id = await monRingDb.saveRing(test_data.rings[0]);
+      final id = await monRingDb.saveRing(mock_data.rings[0]);
       final ring = await monRingDb.getRing(id);
 
       expect(ring.id, id);
       expect(ring.sessionId, 1);
+      expect(ring.ringerId.toString(), '6000');
       expect(ring.primaryIdMethod.toString(), 'A0');
-      expect(ring.ringSeriesCode.toString(), 'C');
-      expect(ring.ringIdNumber.toString(), '12');
+      expect(ring.ringSeriesCode.toString(), 'T');
+      expect(ring.ringIdNumber.toString(), '44');
       expect(ring.metalRingInfo.toString(), '1');
-      expect(ring.species.toString(), 'SYLATR');
+      expect(ring.species.toString(), 'ANA.CLY');
       expect(ring.age.toString(), '3');
       expect(ring.sex.toString(), 'F');
       expect(ring.sexingMethod.toString(), 'S');
@@ -176,50 +183,51 @@ void main() {
     });
 
     test('Anogher ring data can be saved, updated and deleted.', () async {
-      final id = await monRingDb.saveRing(test_data.rings[1]);
+      final id = await monRingDb.saveRing(mock_data.rings[1]);
       final ring = await monRingDb.getRing(id);
 
       expect(ring.id, id);
-      expect(ring.sessionId, 2);
-      expect(ring.primaryIdMethod.toString(), 'B0');
-      expect(ring.ringSeriesCode.toString(), 'D');
-      expect(ring.ringIdNumber.toString(), '14');
-      expect(ring.metalRingInfo.toString(), '2');
-      expect(ring.species.toString(), 'EMBLLA');
-      expect(ring.age.toString(), '4');
-      expect(ring.sex.toString(), 'M');
-      expect(ring.sexingMethod.toString(), 'U');
-      expect(ring.otherMarks.toString(), 'BB');
-      expect(ring.catchingMethod.toString(), 'A');
-      expect(ring.catchingLures.toString(), 'F');
-      expect(ring.condition.toString(), '6');
-      expect(ring.circumstances.toString(), '44');
-      expect(ring.circumstancesPresumed.toString(), '1');
-      expect(ring.status.toString(), 'N');
-      expect(ring.time.toString(), '05:55');
-      expect(ring.wingLength.toString(), '142.2');
-      expect(ring.thirdPrimary.toString(), '80.5');
-      expect(ring.wingPointState.toString(), 'U');
-      expect(ring.mass.toString(), '32.5');
-      expect(ring.moult.toString(), 'P');
-      expect(ring.plumageCode.toString(), 'D');
-      expect(ring.hindClaw.toString(), '22.1');
-      expect(ring.billLength.toString(), '30.2');
-      expect(ring.billMethod.toString(), 'S');
-      expect(ring.headLengthTotal.toString(), '43.5');
-      expect(ring.tarsus.toString(), '36.7');
-      expect(ring.tarsusMethod.toString(), 'S');
-      expect(ring.tailLength.toString(), '55.5');
-      expect(ring.tailDifference.toString(), '6.5');
-      expect(ring.fatScore.toString(), '3');
-      expect(ring.fatScoreMethod.toString(), 'E');
-      expect(ring.pectoralMuscle.toString(), '2');
-      expect(ring.broodPatch.toString(), '3');
-      expect(ring.primaryScore.toString(), '55');
-      expect(ring.primaryMoult.toString(), '1');
-      expect(ring.oldGreaterCoverts.toString(), '9');
-      expect(ring.alula.toString(), '0');
-      expect(ring.carpalCovert.toString(), '0');
+      expect(ring.sessionId, 1);
+      expect(ring.ringerId.toString(), '6000');
+      expect(ring.primaryIdMethod.toString(), 'A0');
+      expect(ring.ringSeriesCode.toString(), 'C');
+      expect(ring.ringIdNumber.toString(), '12');
+      expect(ring.metalRingInfo.toString(), '1');
+      expect(ring.species.toString(), 'SYL.ATR');
+      expect(ring.age.toString(), '3');
+      expect(ring.sex.toString(), 'F');
+      expect(ring.sexingMethod.toString(), 'S');
+      expect(ring.otherMarks.toString(), 'ZZ');
+      expect(ring.catchingMethod.toString(), 'M');
+      expect(ring.catchingLures.toString(), 'N');
+      expect(ring.condition.toString(), '8');
+      expect(ring.circumstances.toString(), '61');
+      expect(ring.circumstancesPresumed.toString(), '0');
+      expect(ring.status.toString(), 'U');
+      expect(ring.time.toString(), '04:55');
+      expect(ring.wingLength.toString(), '122.2');
+      expect(ring.thirdPrimary.toString(), '70.5');
+      expect(ring.wingPointState.toString(), 'A');
+      expect(ring.mass.toString(), '22.5');
+      expect(ring.moult.toString(), 'M');
+      expect(ring.plumageCode.toString(), 'A');
+      expect(ring.hindClaw.toString(), '12.1');
+      expect(ring.billLength.toString(), '20.2');
+      expect(ring.billMethod.toString(), 'N');
+      expect(ring.headLengthTotal.toString(), '33.5');
+      expect(ring.tarsus.toString(), '26.7');
+      expect(ring.tarsusMethod.toString(), 'M');
+      expect(ring.tailLength.toString(), '45.5');
+      expect(ring.tailDifference.toString(), '5.5');
+      expect(ring.fatScore.toString(), '2');
+      expect(ring.fatScoreMethod.toString(), 'B');
+      expect(ring.pectoralMuscle.toString(), '1');
+      expect(ring.broodPatch.toString(), '2');
+      expect(ring.primaryScore.toString(), '45');
+      expect(ring.primaryMoult.toString(), '0');
+      expect(ring.oldGreaterCoverts.toString(), 'A');
+      expect(ring.alula.toString(), '1');
+      expect(ring.carpalCovert.toString(), '1');
 
       final updated = await monRingDb.updateRing(
         RingEntityCompanion(
@@ -230,7 +238,7 @@ void main() {
           ringSeriesCode: const d.Value('E'),
           ringIdNumber: const d.Value('16'),
           metalRingInfo: const d.Value('3'),
-          species: const d.Value('MILVUS'),
+          species: const d.Value('MIL.VUS'),
           age: const d.Value('5'),
           sex: const d.Value('U'),
           sexingMethod: const d.Value('A'),
@@ -271,11 +279,12 @@ void main() {
 
       expect(updated, true);
       expect(updatedRing.sessionId, 3);
+      expect(updatedRing.ringerId.toString(), '7000');
       expect(updatedRing.primaryIdMethod.toString(), 'C0');
       expect(updatedRing.ringSeriesCode.toString(), 'E');
       expect(updatedRing.ringIdNumber.toString(), '16');
       expect(updatedRing.metalRingInfo.toString(), '3');
-      expect(updatedRing.species.toString(), 'MILVUS');
+      expect(updatedRing.species.toString(), 'MIL.VUS');
       expect(updatedRing.age.toString(), '5');
       expect(updatedRing.sex.toString(), 'U');
       expect(updatedRing.sexingMethod.toString(), 'A');
@@ -317,30 +326,32 @@ void main() {
     });
 
     test('Stream emits list of right rings.', () async {
-      await monRingDb.saveRing(test_data.rings[0]);
-      await monRingDb.saveRing(test_data.rings[1]);
+      await monRingDb.saveRing(mock_data.rings[0]);
+      await monRingDb.saveRing(mock_data.rings[1]);
 
       final expectation = expectLater(
           monRingDb.watchSessionRings(2).map((ringList) => ringList.length),
-          emitsInOrder([1, 2]));
+          emitsInOrder([0, 1]));
 
-      await monRingDb.saveRing(test_data.rings[2]);
+      await monRingDb.saveRing(mock_data.rings[2]);
       await expectation;
     });
   });
 
   group('Retrap data can be saved, updated, retrieved and deleted.', () {
     test('Retrap data can be saved.', () async {
-      final id = await monRingDb.saveRetrap(test_data.retraps[0]);
+      final id = await monRingDb.saveRetrap(mock_data.retraps[0]);
       final retrap = await monRingDb.getRetrap(id);
 
       expect(retrap.id, id);
       expect(retrap.sessionId, 1);
+      expect(retrap.ringerId.toString(), '6000');
+      expect(retrap.ringScheme.toString(), 'GBT');
       expect(retrap.primaryIdMethod.toString(), 'A0');
       expect(retrap.ringSeriesCode.toString(), 'C');
       expect(retrap.ringIdNumber.toString(), '12');
       expect(retrap.metalRingInfo.toString(), '1');
-      expect(retrap.species.toString(), 'SYLATR');
+      expect(retrap.species.toString(), 'SYL.ATR');
       expect(retrap.age.toString(), '3');
       expect(retrap.sex.toString(), 'F');
       expect(retrap.sexingMethod.toString(), 'S');
@@ -378,16 +389,18 @@ void main() {
     });
 
     test('Anogher retrap data can be saved, updated and deleted.', () async {
-      final id = await monRingDb.saveRetrap(test_data.retraps[1]);
+      final id = await monRingDb.saveRetrap(mock_data.retraps[1]);
       final retrap = await monRingDb.getRetrap(id);
 
       expect(retrap.id, id);
       expect(retrap.sessionId, 2);
+      expect(retrap.ringerId.toString(), '7000');
+      expect(retrap.ringScheme.toString(), 'SKB');
       expect(retrap.primaryIdMethod.toString(), 'B0');
       expect(retrap.ringSeriesCode.toString(), 'D');
       expect(retrap.ringIdNumber.toString(), '14');
       expect(retrap.metalRingInfo.toString(), '2');
-      expect(retrap.species.toString(), 'EMBLLA');
+      expect(retrap.species.toString(), 'EMB.LLA');
       expect(retrap.age.toString(), '4');
       expect(retrap.sex.toString(), 'M');
       expect(retrap.sexingMethod.toString(), 'U');
@@ -426,14 +439,14 @@ void main() {
       final updated = await monRingDb.updateRetrap(
         RetrapEntityCompanion(
           id: d.Value(id),
-          sessionId: const d.Value(3),
+          sessionId: const d.Value(2),
           ringerId: const d.Value('7000'),
-          ringScheme: const d.Value('DEB'),
+          ringScheme: const d.Value('GBT'),
           primaryIdMethod: const d.Value('C0'),
           ringSeriesCode: const d.Value('E'),
           ringIdNumber: const d.Value('16'),
           metalRingInfo: const d.Value('3'),
-          species: const d.Value('MILVUS'),
+          species: const d.Value('MIL.VUS'),
           age: const d.Value('5'),
           sex: const d.Value('U'),
           sexingMethod: const d.Value('A'),
@@ -473,12 +486,14 @@ void main() {
       final updatedRetrap = await monRingDb.getRetrap(id);
 
       expect(updated, true);
-      expect(updatedRetrap.sessionId, 3);
+      expect(updatedRetrap.sessionId, 2);
+      expect(updatedRetrap.ringerId.toString(), '7000');
+      expect(updatedRetrap.ringScheme.toString(), 'GBT');
       expect(updatedRetrap.primaryIdMethod.toString(), 'C0');
       expect(updatedRetrap.ringSeriesCode.toString(), 'E');
       expect(updatedRetrap.ringIdNumber.toString(), '16');
       expect(updatedRetrap.metalRingInfo.toString(), '3');
-      expect(updatedRetrap.species.toString(), 'MILVUS');
+      expect(updatedRetrap.species.toString(), 'MIL.VUS');
       expect(updatedRetrap.age.toString(), '5');
       expect(updatedRetrap.sex.toString(), 'U');
       expect(updatedRetrap.sexingMethod.toString(), 'A');
@@ -520,8 +535,8 @@ void main() {
     });
 
     test('Stream emits list of right retraps.', () async {
-      await monRingDb.saveRetrap(test_data.retraps[0]);
-      await monRingDb.saveRetrap(test_data.retraps[1]);
+      await monRingDb.saveRetrap(mock_data.retraps[0]);
+      await monRingDb.saveRetrap(mock_data.retraps[1]);
 
       final expectation = expectLater(
           monRingDb
@@ -529,7 +544,7 @@ void main() {
               .map((retrapList) => retrapList.length),
           emitsInOrder([1, 2]));
 
-      await monRingDb.saveRetrap(test_data.retraps[2]);
+      await monRingDb.saveRetrap(mock_data.retraps[2]);
       await expectation;
     });
   });
